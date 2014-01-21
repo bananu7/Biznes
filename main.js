cc0c5dd988f9901dc4088310053a07e51eca0b53
Bartek Banachewicz <bananu7@o2.pl>
2014-01-21 18:02:14 +0100

    JSON

--- main.js
+++ main.js
@@ -6,13 +6,33 @@
 String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
 
 var addItem;
+var showData;
 
+//var item = function (t, d, p) { addItem({ title: t, description: d, price: "$" + p }); };
+var itemData = [
+    { 
+        title: "GeForce GTX 670",
+        description: "NVidia GPU",
+        dataSets: [
+            { name: "Ceneo.pl", data: [65, 59, 90, 81, 56, 55, 40] }
+        ]
+    },
+    { 
+        title: "Radeon 9700",
+        description: "AMD GPU",
+        dataSets: []
+    },
+    { 
+        title: "Core i7",
+        description: "Intel CPU",
+        dataSets: []
+    }
+];
 
 // Code
 $(function(){
-
     addItem = function() {
-    var source   = $("#item-template").html();
+        var source   = $("#item-template").html();
         var template = Handlebars.compile(source);
 
         return function(item) {
@@ -20,6 +40,35 @@ $(function(){
             $("#items").append(itemHtml);
         }
     }();
+    showData = function(item) {
+        var details = $("#itemDetails");
+        var lineChartData = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    fillColor: "rgba(0, 82, 89, 0.8)",
+                    strokeColor: "#F4E7BA",
+                    pointColor: "#F4E7BA",
+                    pointStrokeColor: "rgba(0, 82, 89, 0.8)",
+                    data: [65, 59, 90, 81, 56, 55, 40]
+                },
+                {
+                    fillColor: "rgba(95, 138, 131, 0.5)",
+                    strokeColor: "rgba(0, 82, 89, 1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    data: [28, 48, 40, 19, 96, 27, 100]
+                }
+            ]
+        };
+
+        var options = {
+            scaleFontColor: "#F4E7BA",
+            scaleGridLineColor: "#F4E7BA"
+        };
+
+        var myLine = new Chart(document.getElementById("itemChart").getContext("2d")).Line(lineChartData, options);
+    };
 
 
     $("#searchBox").on("input", function(){
@@ -40,15 +89,12 @@ $(function(){
         items.hide();
     });
 
-    // dummy items
+    // dummy item
 
-    var item = function (t, d, p) { addItem({ title: t, description: d, price: "$" + p }); };
+    $("div.item").click(function () {
+        showData()
+    });
 
-    item("GeForce GTX 670", "NVidia GPU", 1100);
-    item("Radeon 9700", "AMD GPU", 900);
-    item("Core i7", "Intel CPU", 1400);
-
-    
     var resize = function () {
         // set canvas to its actual pixel size
         var itemChart = document.getElementById("itemChart");
@@ -57,34 +103,4 @@ $(function(){
     };
     $(window).resize(resize);
     resize();
-
-    //$("div.item").click(function () {
-        var details = $("#itemDetails");
-        var lineChartData = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    fillColor: "rgba(0, 82, 89, 0.8)",
-                    strokeColor: "#F4E7BA",
-                    pointColor: "#F4E7BA",
-                    pointStrokeColor: "rgba(0, 82, 89, 0.8)",
-                    data: [65, 59, 90, 81, 56, 55, 40]
-                },
-                {
-                    fillColor: "rgba(95, 138, 131, 0.5)",
-                    strokeColor: "rgba(0, 82, 89, 1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    data: [28, 48, 40, 19, 96, 27, 100]
-                }
-            ]
-        }
-
-        var options = {
-            scaleFontColor: "#F4E7BA",
-            scaleGridLineColor: "#F4E7BA"
-        };
-
-        var myLine = new Chart(document.getElementById("itemChart").getContext("2d")).Line(lineChartData, options);
-   // });
 });
