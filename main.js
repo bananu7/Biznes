43ec2574a0b66263ac4f2fd611bdd9bd6fc99ec7
Bartek Banachewicz <bananu7@o2.pl>
2014-01-21 22:36:19 +0100

    Added datasets loading

--- main.js
+++ main.js
@@ -1,74 +1,105 @@
+"use strict";
 /**
  * Created by Bajtek on 1/13/14.
  */
 
 // Stuff
-String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
-
-var addItem;
-var showData;
+String.prototype.contains = function (it) { return this.indexOf(it) != -1; };
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function getRandomIntTable(min, max, length) {
+    var t = [];
+    for (var i = 0; i < length; i++) {
+        t.push(getRandomInt(min, max));
+    }
+    return t;
+}
 
 //var item = function (t, d, p) { addItem({ title: t, description: d, price: "$" + p }); };
-var itemData = [
-    { 
+var items = [
+    {
         title: "GeForce GTX 670",
         description: "NVidia GPU",
         dataSets: [
-            { name: "Ceneo.pl", data: [65, 59, 90, 81, 56, 55, 40] }
+            { name: "Ceneo.pl", data: getRandomIntTable(1000, 1300, 7) }
         ]
     },
-    { 
+    {
         title: "Radeon 9700",
         description: "AMD GPU",
-        dataSets: []
+        dataSets: [
+            { name: "Ceneo.pl", data: getRandomIntTable(900, 1500, 7) }
+        ]
     },
-    { 
+    {
         title: "Core i7",
         description: "Intel CPU",
-        dataSets: []
+        dataSets: [
+            { name: "Ceneo.pl", data: getRandomIntTable(1200, 1600, 7) }
+        ]
     }
 ];
 
 // Code
 $(function(){
-    addItem = function() {
-        var source   = $("#item-template").html();
-        var template = Handlebars.compile(source);
+    function loadItems() {
+        var showData = function (item) {
+            var details = $("#itemDetails");
 
-        return function(item) {
-            var itemHtml = template(item);
-            $("#items").append(itemHtml);
+            var datasetsTemplates = [
+                    {
+                        fillColor: "rgba(0, 82, 89, 0.8)",
+                        strokeColor: "#F4E7BA",
+                        pointColor: "#F4E7BA",
+                        pointStrokeColor: "rgba(0, 82, 89, 0.8)",
+                    },
+                    {
+                        fillColor: "rgba(95, 138, 131, 0.5)",
+                        strokeColor: "rgba(0, 82, 89, 1)",
+                        pointColor: "rgba(151,187,205,1)",
+                        pointStrokeColor: "#fff",
+                    }
+            ];
+
+            var lineChartData = {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: []
+            };
+
+            for (var i = 0; i < item.dataSets.length; i++) {
+                var set = datasetsTemplates[i % datasetsTemplates.length];
+                set.data = item.dataSets[i].data;
+
+                lineChartData.datasets.push(set);
+            }
+
+            var options = {
+                scaleFontColor: "#F4E7BA",
+                scaleGridLineColor: "#F4E7BA"
+            };
+
+            var myLine = new Chart(document.getElementById("itemChart").getContext("2d")).Line(lineChartData, options);
+        };
+
+        var addItem = function () {
+            var source = $("#item-template").html();
+            var template = Handlebars.compile(source);
+
+            return function (item) {
+                var itemHtml = template(item);
+                var itemJQ = $(itemHtml);
+                $("#items").append(itemJQ);
+                itemJQ.click(function () {
+                    showData(item);
+                });
+            }
+        }();
+
+        for (var i = 0; i < items.length; i++) {
+            addItem(items[i]);
         }
-    }();
-    showData = function(item) {
-        var details = $("#itemDetails");
-        var lineChartData = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    fillColor: "rgba(0, 82, 89, 0.8)",
-                    strokeColor: "#F4E7BA",
-                    pointColor: "#F4E7BA",
-                    pointStrokeColor: "rgba(0, 82, 89, 0.8)",
-                    data: [65, 59, 90, 81, 56, 55, 40]
-                },
-                {
-                    fillColor: "rgba(95, 138, 131, 0.5)",
-                    strokeColor: "rgba(0, 82, 89, 1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    data: [28, 48, 40, 19, 96, 27, 100]
-                }
-            ]
-        };
-
-        var options = {
-            scaleFontColor: "#F4E7BA",
-            scaleGridLineColor: "#F4E7BA"
-        };
-
-        var myLine = new Chart(document.getElementById("itemChart").getContext("2d")).Line(lineChartData, options);
-    };
+    }
 
 
     $("#searchBox").on("input", function(){
@@ -89,11 +120,7 @@ $(function(){
         items.hide();
     });
 
-    // dummy item
-
-    $("div.item").click(function () {
-        showData()
-    });
+    loadItems();
 
     var resize = function () {
         // set canvas to its actual pixel size
